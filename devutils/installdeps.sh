676200ab682139d8b1282ec1b8a8ff5c0f9db2b7
fmenez <fmenez@.(none)>
2015-08-18 13:12:14 +0200

    ADD: util script to go get dependencies; will be dropped when we will have switched to a vendoring solution (eg. godep or gb)

---
+++ devutils/installdeps.sh
@@ -0,0 +1,6 @@
+go get github.com/dullgiulio/pingo
+go get github.com/go-sql-driver/mysql
+go get github.com/gorilla/rpc
+go get github.com/gorilla/rpc/json
+go get github.com/hypersleep/easyssh
+go get golang.org/x/net/icmp
