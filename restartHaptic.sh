f3a30d88f724d58ae2e121b678f629ae5e8f9e49
Romain Soufflet <romain@soufflet.io>
2015-10-13 19:42:24 +0200

    NC-141 Add licence text in each source files

--- restartHaptic.sh
+++ restartHaptic.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+# Nanocloud community -- transform any application into SaaS solution
+#
+# Copyright (C) 2015 Nanocloud Software
+#
+# This file is part of Nanocloud community.
+#
+# Nanocloud community is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# Nanocloud community is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 ZEROINSTAL_DIR="/home/gentux/projects/gocode/src/nanocloud.com/zeroinstall"
 HAPTIC_SRC_DIR="${ZEROINSTAL_DIR}/agent/haptic"
 PLUGINS_SRC_DIR="${ZEROINSTAL_DIR}/plugins"
--- userPassword.sh
+++ userPassword.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+# Nanocloud community -- transform any application into SaaS solution
+#
+# Copyright (C) 2015 Nanocloud Software
+#
+# This file is part of Nanocloud community.
+#
+# Nanocloud community is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# Nanocloud community is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 # Configuration
 MYPATH=/home/nanosoft/0.2
 WINEXE=$MYPATH'/winexe-static -U 'intra.nanocloud.com/Administrator%password' --runas='intra.nanocloud.com/Administrator%password' //10.20.12.20'
